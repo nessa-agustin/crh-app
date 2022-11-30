dd1277213d6c6efa01bab39234789810facff3eb
Nessa Agustin <nessa.agustin@gmail.com>
2022-12-01 02:50:22 +0800

    add logic and authentication

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,47 @@
-import '../App.css';
-
+// import '../App.css';
+import { useState } from "react";
+import {useNavigate} from 'react-router-dom';
 
 function Login() {
+
+    const navigate = useNavigate();
+    let userInit = {username: "", password: ""};
+    const [user, setUser] = useState(userInit);
+
+
+    function submitForm(e){
+       e.preventDefault();
+
+        if(user.username === "testuser" && user.password === "password"){
+            localStorage.setItem("auth", true);
+            navigate('/list')
+        }else{
+            alert("Invalid credentials")
+            setUser(userInit)
+        }
+
+
+    }
+
+    function getChanges(field){
+        return (e) => setUser((user) => ({...user, [field]: e.target.value}))
+    }
+
+
 
 
     return (
         <div className="container">
 
-            <form>
+            <form onSubmit={(e) => submitForm(e)}>
                 <label>
                     Username:
-                    <input type="text" name="name" placeholder='Username' />
+                    <input type="text" value={user.username} placeholder='Username' onChange={getChanges('username')}/>
                 </label>
                 <br />
                 <label>
                     Password:
-                    <input type="password" name="password" placeholder='Password' />
+                    <input type="password" value={user.password} placeholder='Password' onChange={getChanges('password')}/>
                 </label>
                 <br />
                 <input type="submit" value="Login" />
--- src/pages/UserList.js
+++ src/pages/UserList.js
@@ -1,23 +1,49 @@
+import { useState, useEffect } from "react";
+
+function UserList() {
+
+    const [userData, setUserdata] = useState("");
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                let users = data.map((user) => {
+                    return (
+                        <tr key={user.id}>
+                            <td>{user.name}</td>
+                            <td>{user.username}</td>
+                            <td>{user.phone}</td>
+                            <td>{user.company.name}</td>
+                        </tr>
+                    )
+                })
+                setUserdata(users);
+            })
+    }, [])
 
 
-function UserList(){
+
 
 
     return (
 
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Username</th>
-                    <th>Phone</th>
-                    <th>Company Name</th>
-                </tr>
-            </thead>
-            <tbody>
-
-            </tbody>
-        </table>
+        <div className="container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Username</th>
+                        <th>Phone</th>
+                        <th>Company Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {userData}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
