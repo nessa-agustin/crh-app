3a572fff94bd3671908451f3b665f2ab8ce63c4f
Nessa Agustin <nessa.agustin@gmail.com>
2022-11-30 15:14:17 +0800

    create form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <form className=''>
+        <label>
+          Username:
+          <input type="text" name="name" placeholder='Username'/>
+        </label>
+        <br/>
+        <label>
+          Password:
+          <input type="password" name="password" placeholder='Password'/>
+        </label>
+        <br/>
+        <input type="submit" value="Login" />
+      </form>
+
+
+
     </div>
   );
 }
