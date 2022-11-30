61a08808c25070fa6c822a409277a01237e5830a
Nessa Agustin <nessa.agustin@gmail.com>
2022-12-01 05:58:21 +0800

    apply route filtering

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import UserList from './pages/UserList';
@@ -7,15 +7,18 @@ import UserList from './pages/UserList';
 
 function App() {
 
-
+  let auth = localStorage.getItem("auth");
 
   return (
-   <Router>
+    <Router>
       <Routes>
         <Route path='/' element={<Login />} />
-        <Route path='/list' element={<UserList />} />
+        {
+          auth &&
+          <Route path='/list' element={<UserList />} />
+        }
       </Routes>
-   </Router>
+    </Router>
   );
 }
 
